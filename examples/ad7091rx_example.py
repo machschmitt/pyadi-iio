9ce045e54bc57b51a8cf68c38c5ba07e24d12c81
Marcelo Schmitt <marcelo.schmitt@analog.com>
2023-05-30 11:39:49 -0300

    Update ad7091rx_example
    Remove comments that don't apply to ad7091rx.
    Add device name parameter.
    Remove temperature channel test since ad7091rx don't have temp channel.
    
    Signed-off-by: Marcelo Schmitt <marcelo.schmitt@analog.com>

--- examples/ad7091rx_example.py
+++ examples/ad7091rx_example.py
@@ -37,46 +37,31 @@ import adi
 
 # Optionally pass URI as command line argument, else use analog.local
 # (URI stands for "Uniform Resource Identifier")
-# NOTE - when running directly on the Raspberry Pi, you CAN use "local",
-# but you must run as root (sudo) because we are writing as well as reading
+# Optionally pass device name argument.
 my_uri = sys.argv[1] if len(sys.argv) >= 2 else "ip:analog.local"
-print("uri: " + str(my_uri))
+dev_name = sys.argv[2] if len(sys.argv) >= 3 else "ad7091r-8"
 
-# Set up AD5592R/AD5593R
-my_ad7091rx = adi.ad7091rx(uri=my_uri, device_name="ad7091rx")
+print("uri: " + str(my_uri))
+print("dev_name: " + str(dev_name))
+
+# Set up AD7091R-2/AD7091R-4/AD7091R-8 from specific uri address and device name.
+my_ad7091rx = adi.ad7091rx(uri=my_uri, device_name=dev_name)
 
 # Create iterable list of channels. This is only for test purposes in the example,
 # where the channel configuration is not known ahead of time.
-# In an actual application, the channels are configured in the device tree
-# and are known ahead of time. See ./examples/ad5592r_examples/ad5592r_curve_tracer.py.
-
 channels = []
 for attr in dir(my_ad7091rx):
     if type(getattr(my_ad7091rx, attr)) in (adi.ad7091rx._channel_adc,):
         channels.append(getattr(my_ad7091rx, attr))
 
-# Write votalge value for each channel
-for ch in channels:
-    if ch._output:  # Only write if it is an output channel
-        mV = input(f"Enter desired voltage for channel {ch.name} in mV: ")
-        # ch(mV)  # Use channel's call method to conveniently set in mV
-
 # Read each channels and its parameters
 for ch in channels:
     print("***********************")  # Just a separator for easier serial read
     print("Channel Name: ", ch.name)  # Channel Name
-    print("is Output? ", ch._output)  # True = Output/Write/DAC, False = Input/Read/ADC
     print("Channel Scale: ", ch.scale)  # Channel Scale is Vref/2^12
     print("Channel Raw Value: ", ch.raw)  # Channel Raw Value
 
-    # Print Temperature in Celsius
-    if ch.name == "temp":
-        print("Channel Offset Value: ", ch.offset)  # Channel Offset Value
-        print(
-            "Channel Temperature (C): ", ch()
-        )  # Use channel's call method, which returns degrees C.
     # Print Real Voltage in mV
-    else:
-        print(
-            "Channel Real Value (mV): ", ch()
-        )  # Use channel's call method, which returns millivolts.
+    print(
+        "Channel Real Value (mV): ", ch()
+    )  # Use channel's call method, which returns millivolts.
